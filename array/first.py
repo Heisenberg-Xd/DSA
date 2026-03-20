aab1b22780e52f71a9e0d21101b4e622655ce49c
Heisenberg-Xd <kartikeshgaonkar2509@gmail.com>
2026-03-20 21:23:34 +0530

    finding second largest

--- array/first.py
+++ array/first.py
@@ -1,1 +1,13 @@
 #finding the second largest number in array
+
+def Getsecondlargest(arr):
+
+    n =len(arr)
+    arr.sort()
+
+    return arr[n-2]
+
+if __name__ == "__main__":
+    arr=[2,7,1,6,9,5]
+
+    print(Getsecondlargest(arr))
