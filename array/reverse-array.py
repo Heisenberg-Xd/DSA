d11e35eb003f7e108328c1ec7c237786582e044a
Heisenberg-Xd <kartikeshgaonkar2509@gmail.com>
2026-03-21 20:19:59 +0530

    reverse an array

---
+++ array/reverse-array.py
@@ -0,0 +1,16 @@
+def reverseArray(arr):
+    n=len(arr)
+
+    temp =[0] * n
+    
+    for i in range(n):
+        temp[i]=arr[n-i-1]
+
+    for i in range(n):
+        arr[i]=temp[i]
+
+if __name__ == "__main__":
+    arr=[9,8,7,6,5,4,3,2,1]
+
+for i in range(len(arr)):
+    print(arr[i], end = ' ')
