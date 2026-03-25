cc76a52a97551ede466f093cbd9184bd0d1a5bed
Heisenberg-Xd <kartikeshgaonkar2509@gmail.com>
2026-03-25 23:43:39 +0530

    Stock Buy and Sell (Max one transaction)

---
+++ array/buy-sell-stocks.py
@@ -0,0 +1,14 @@
+def MaxProfit(prices):
+    n=len(prices)
+    res=0
+
+    for i in range(n-1):
+        for j in range(i+1,n):
+            res = max(res,prices[j]- prices[i])
+
+    return res 
+
+if __name__ == "__main__":
+    price=[230,430,120,100,800,760]
+
+    print(MaxProfit(price))
