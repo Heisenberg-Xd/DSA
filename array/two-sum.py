e3f042bf0903de5e2046f84c866acb6af5d0a81c
Heisenberg-Xd <kartikeshgaonkar2509@gmail.com>
2026-03-29 00:48:01 +0530

    check if there exists a pair whose sum equals the target

---
+++ array/two-sum.py
@@ -0,0 +1,19 @@
+def twosums(arr,target):
+    n=len(arr)
+
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[i]+arr[j] == target:
+                return True
+        
+    return False
+
+if __name__=="__main__":
+    arr=[1,2,3,4,5,6,7,8,9]
+    target=18
+
+if twosums(arr,target):
+    print ("True")
+else:
+    print ("false")
+        
